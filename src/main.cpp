e3ee8086829a246be393b6b44365f7076fb7786c
Antti Salonen <ajsalonen@gmail.com>
2010-11-20 16:23:38 +0100

    add camera scrolling

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <vector>
+#include <utility>
+
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
 
@@ -28,7 +31,7 @@ map::map(int x, int y)
 	this->data = new int[x * y];
 	for(int i = 0; i < y; i++) {
 		for(int j = 0; j < x; j++) {
-			data[get_index(j, i)] = 1;
+			data[get_index(j, i)] = (i * j) % 5 % 2;
 		}
 	}
 }
@@ -58,25 +61,32 @@ struct camera {
 class gui
 {
 	public:
-		gui(int x, int y);
+		gui(int x, int y, const map& mm, const std::vector<const char*>& terrain_files);
 		~gui();
-		int display(const map& m);
+		int display() const;
+		int handle_keydown(SDLKey k);
 		camera cam;
 	private:
-		int show_terrain_image(int x, int y, int val);
+		int show_terrain_image(int x, int y, int xpos, int ypos) const;
+		const map& m;
 		SDL_Surface* screen;
-		SDL_Surface* terrain1;
+		std::vector<SDL_Surface*> terrains;
 		const int tile_w;
 		const int tile_h;
 		const int screen_w;
 		const int screen_h;
+		const int total_tiles_x;
+		const int total_tiles_y;
 };
 
-gui::gui(int x, int y)
-	: tile_w(32),
+gui::gui(int x, int y, const map& mm, const std::vector<const char*>& terrain_files)
+	: m(mm),
+	tile_w(32),
 	tile_h(32),
 	screen_w(x),
-	screen_h(y)
+	screen_h(y),
+	total_tiles_x((screen_w + tile_w - 1) / tile_w),
+	total_tiles_y((screen_h + tile_h - 1) / tile_h)
 {
 	screen = SDL_SetVideoMode(x, y, 32, SDL_SWSURFACE);
 	if (!screen) {
@@ -87,35 +97,49 @@ gui::gui(int x, int y)
 		fprintf(stderr, "screen is locked\n");
 		SDL_UnlockSurface(screen);
 	}
-	terrain1 = IMG_Load("share/terrain1.png");
-	if (!terrain1) {
-		fprintf(stderr, "Unable to load terrain1: %s\n", IMG_GetError());
+	terrains.resize(terrain_files.size());
+	for(unsigned int i = 0; i < terrain_files.size(); i++) {
+		terrains[i] = IMG_Load(terrain_files[i]);
+		if(!terrains[i]) {
+			fprintf(stderr, "Unable to load terrain '%s': %s\n", terrain_files[i],
+				       	IMG_GetError());
+		}
 	}
 	cam.cam_x = cam.cam_y = 0;
 }
 
 gui::~gui()
 {
-	SDL_FreeSurface(terrain1);
+	for(unsigned int i = 0; i < terrains.size(); i++) {
+		SDL_FreeSurface(terrains[i]);
+	}
 }
 
-int gui::show_terrain_image(int x, int y, int val)
+int gui::show_terrain_image(int x, int y, int xpos, int ypos) const
 {
 	SDL_Rect dest;
-	dest.x = x * tile_w;
-	dest.y = y * tile_h;
-	if(SDL_BlitSurface(terrain1, NULL, screen, &dest)) {
+	dest.x = xpos * tile_w;
+	dest.y = ypos * tile_h;
+	int val = m.get_data(x, y);
+	if(val < 0 || val >= (int)terrains.size()) {
+		fprintf(stderr, "Terrain at %d not loaded at (%d, %d)\n", val,
+				x, y);
+		return 1;
+	}
+	if(SDL_BlitSurface(terrains[val], NULL, screen, &dest)) {
 		fprintf(stderr, "Unable to blit surface: %s\n", SDL_GetError());
 		return 1;
 	}
 	return 0;
 }
 
-int gui::display(const map& m)
+int gui::display() const
 {
-	for(int i = cam.cam_y; i < m.size_y; i++) {
-		for(int j = cam.cam_x; j < m.size_x; j++) {
-			if(show_terrain_image(i, j, m.get_data(i, j))) {
+	int imax = std::min(cam.cam_y + total_tiles_y, m.size_y);
+	int jmax = std::min(cam.cam_x + total_tiles_x, m.size_x);
+	for(int i = cam.cam_y, y = 0; i < imax; i++, y++) {
+		for(int j = cam.cam_x, x = 0; j < jmax; j++, x++) {
+			if(show_terrain_image(j, i, x, y)) {
 				return 1;
 			}
 		}
@@ -127,13 +151,59 @@ int gui::display(const map& m)
 	return 0;
 }
 
+int gui::handle_keydown(SDLKey k)
+{
+	bool redraw = false;
+	switch(k) {
+		case SDLK_LEFT:
+			if(cam.cam_x > 0)
+				cam.cam_x--, redraw = true;
+			break;
+		case SDLK_RIGHT:
+			if(cam.cam_x < m.size_x - total_tiles_x - 1)
+				cam.cam_x++, redraw = true;
+			break;
+		case SDLK_UP:
+			if(cam.cam_y > 0)
+				cam.cam_y--, redraw = true;
+			break;
+		case SDLK_DOWN:
+			if(cam.cam_y < m.size_y - total_tiles_y - 1)
+				cam.cam_y++, redraw = true;
+			break;
+		case SDLK_ESCAPE:
+		case SDLK_q:
+			return 1;
+		default:
+			break;
+	}
+	if(redraw)
+		display();
+	return 0;
+}
+
 int run()
 {
-	gui g(640, 480);
-	map m(20, 20);
-
-	g.display(m);
-	sleep(2);
+	std::vector<const char*> terrain_files;
+	terrain_files.push_back("share/terrain1.png");
+	terrain_files.push_back("share/terrain2.png");
+	map m(64, 64);
+	gui g(640, 480, m, terrain_files);
+	g.display();
+	while(1) {
+		SDL_Event event;
+		SDL_WaitEvent(&event);
+		switch(event.type) {
+			case SDL_KEYDOWN:
+				if(g.handle_keydown(event.key.keysym.sym))
+					return 0;
+				break;
+			case SDL_QUIT:
+				return 0;
+			default:
+				break;
+		}
+	}
 }
 
 int main(int argc, char **argv)
@@ -145,6 +215,7 @@ int main(int argc, char **argv)
 	if(!IMG_Init(IMG_INIT_PNG)) {
 		fprintf(stderr, "Unable to init SDL_Image: %s\n", IMG_GetError());
 	}
+	SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
 	run();
 
 	SDL_Quit();
