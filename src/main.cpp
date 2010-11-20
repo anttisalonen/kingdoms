91c50231c80cbd74968f3da8c1aed33aa857af81
Antti Salonen <ajsalonen@gmail.com>
2010-11-20 17:51:48 +0100

    add drawing units

--- src/main.cpp
+++ src/main.cpp
@@ -12,52 +12,6 @@ bool in_bounds(int a, int b, int c)
 	return a <= b && b <= c;
 }
 
-class map {
-	public:
-		map(int x, int y);
-		~map();
-		int get_data(int x, int y) const;
-		const int size_x;
-		const int size_y;
-	private:
-		int get_index(int x, int y) const;
-		int *data;
-};
-
-map::map(int x, int y)
-	: size_x(x),
-	size_y(y)
-{
-	this->data = new int[x * y];
-	for(int i = 0; i < y; i++) {
-		for(int j = 0; j < x; j++) {
-			data[get_index(j, i)] = (i * j) % 5 % 2;
-		}
-	}
-}
-
-map::~map()
-{
-	delete[] this->data;
-}
-
-int map::get_index(int x, int y) const
-{
-	return y * size_x + x;
-}
-
-int map::get_data(int x, int y) const
-{
-	if(!in_bounds(0, x, size_x - 1) || !in_bounds(0, y, size_y - 1))
-		return -1;
-	return data[get_index(x, y)];
-}
-
-struct camera {
-	int cam_x;
-	int cam_y;
-};
-
 struct color {
 	int r;
 	int g;
@@ -70,113 +24,6 @@ color::color(int r_, int g_, int b_)
 	g(g_),
 	b(b_)
 {
-}
-
-class gui
-{
-	public:
-		gui(int x, int y, const map& mm, const std::vector<const char*>& terrain_files);
-		~gui();
-		int display() const;
-		int handle_keydown(SDLKey k);
-		camera cam;
-	private:
-		int show_terrain_image(int x, int y, int xpos, int ypos) const;
-		int draw_main_map() const;
-		int draw_sidebar() const;
-		color get_minimap_color(int x, int y) const;
-		const map& m;
-		SDL_Surface* screen;
-		std::vector<SDL_Surface*> terrains;
-		const int tile_w;
-		const int tile_h;
-		const int screen_w;
-		const int screen_h;
-		const int cam_total_tiles_x;
-		const int cam_total_tiles_y;
-		const int sidebar_size;
-};
-
-gui::gui(int x, int y, const map& mm, const std::vector<const char*>& terrain_files)
-	: m(mm),
-	tile_w(32),
-	tile_h(32),
-	screen_w(x),
-	screen_h(y),
-	cam_total_tiles_x((screen_w + tile_w - 1) / tile_w),
-	cam_total_tiles_y((screen_h + tile_h - 1) / tile_h),
-	sidebar_size(4)
-{
-	screen = SDL_SetVideoMode(x, y, 32, SDL_SWSURFACE);
-	if (!screen) {
-		fprintf(stderr, "Unable to set %dx%d video: %s\n", x, y, SDL_GetError());
-		return;
-	}
-	if(screen->locked) {
-		fprintf(stderr, "screen is locked\n");
-		SDL_UnlockSurface(screen);
-	}
-	terrains.resize(terrain_files.size());
-	for(unsigned int i = 0; i < terrain_files.size(); i++) {
-		SDL_Surface* img = IMG_Load(terrain_files[i]);
-		if(!img) {
-			fprintf(stderr, "Unable to load terrain '%s': %s\n", terrain_files[i],
-				       	IMG_GetError());
-		}
-		else {
-			terrains[i] = SDL_DisplayFormat(img);
-			SDL_FreeSurface(img);
-			if(!terrains[i]) {
-				fprintf(stderr, "Unable to convert terrain '%s': %s\n",
-						terrain_files[i], SDL_GetError());
-			}
-		}
-	}
-	cam.cam_x = cam.cam_y = 0;
-}
-
-gui::~gui()
-{
-	for(unsigned int i = 0; i < terrains.size(); i++) {
-		SDL_FreeSurface(terrains[i]);
-	}
-}
-
-int gui::show_terrain_image(int x, int y, int xpos, int ypos) const
-{
-	SDL_Rect dest;
-	dest.x = xpos * tile_w;
-	dest.y = ypos * tile_h;
-	int val = m.get_data(x, y);
-	if(val < 0 || val >= (int)terrains.size()) {
-		fprintf(stderr, "Terrain at %d not loaded at (%d, %d)\n", val,
-				x, y);
-		return 1;
-	}
-	if(SDL_BlitSurface(terrains[val], NULL, screen, &dest)) {
-		fprintf(stderr, "Unable to blit surface: %s\n", SDL_GetError());
-		return 1;
-	}
-	return 0;
-}
-
-int gui::display() const
-{
-	if (SDL_MUSTLOCK(screen)) {
-		if (SDL_LockSurface(screen) < 0) {
-			return 1;
-		}
-	}
-	draw_sidebar();
-	if (SDL_MUSTLOCK(screen)) {
-		SDL_UnlockSurface(screen);
-	}
-	draw_main_map();
-	if(SDL_Flip(screen)) {
-		fprintf(stderr, "Unable to flip: %s\n", SDL_GetError());
-		return 1;
-	}
-	return 0;
 }
 
 /* doesn't update the screen. lock must be held.
@@ -252,6 +99,203 @@ color sdl_get_pixel(SDL_Surface* screen, int x, int y)
 	return color(r, g, b);
 }
 
+SDL_Surface* sdl_load_image(const char* filename)
+{
+	SDL_Surface* img = IMG_Load(filename);
+	if(!img) {
+		fprintf(stderr, "Unable to load image '%s': %s\n", filename,
+				IMG_GetError());
+		return NULL;
+	}
+	else {
+		SDL_Surface* opt = SDL_DisplayFormat(img);
+		SDL_FreeSurface(img);
+		if(!opt) {
+			fprintf(stderr, "Unable to convert image '%s': %s\n",
+					filename, SDL_GetError());
+			return NULL;
+		}
+		else {
+			return opt;
+		}
+	}
+}
+
+class unit
+{
+	public:
+		unit(int uid, int x, int y, const color& c);
+		~unit();
+		int unit_id;
+		int xpos;
+		int ypos;
+};
+
+unit::unit(int uid, int x, int y, const color& c)
+	: unit_id(uid),
+	xpos(x),
+	ypos(y)
+{
+	printf("Constructing unit\n");
+}
+
+unit::~unit()
+{
+	printf("Destructing unit\n");
+}
+
+class map {
+	public:
+		map(int x, int y, const std::vector<unit*>& units_);
+		~map();
+		int get_data(int x, int y) const;
+		const int size_x;
+		const int size_y;
+	private:
+		int get_index(int x, int y) const;
+		int *data;
+	public:
+		std::vector<unit*> units;
+};
+
+map::map(int x, int y, const std::vector<unit*>& units_)
+	: size_x(x),
+	size_y(y),
+	units(units_)
+{
+	this->data = new int[x * y];
+	for(int i = 0; i < y; i++) {
+		for(int j = 0; j < x; j++) {
+			data[get_index(j, i)] = rand() % 2;
+		}
+	}
+}
+
+map::~map()
+{
+	delete[] this->data;
+}
+
+int map::get_index(int x, int y) const
+{
+	return y * size_x + x;
+}
+
+int map::get_data(int x, int y) const
+{
+	if(!in_bounds(0, x, size_x - 1) || !in_bounds(0, y, size_y - 1))
+		return -1;
+	return data[get_index(x, y)];
+}
+
+struct camera {
+	int cam_x;
+	int cam_y;
+};
+
+class gui
+{
+	public:
+		gui(int x, int y, const map& mm, const std::vector<const char*>& terrain_files,
+				const std::vector<const char*>& unit_files);
+		~gui();
+		int display() const;
+		int handle_keydown(SDLKey k);
+		camera cam;
+	private:
+		int show_terrain_image(int x, int y, int xpos, int ypos) const;
+		int draw_main_map() const;
+		int draw_sidebar() const;
+		int draw_unit(const unit& u) const;
+		color get_minimap_color(int x, int y) const;
+		const map& m;
+		SDL_Surface* screen;
+		std::vector<SDL_Surface*> terrains;
+		std::vector<SDL_Surface*> unit_images;
+		const int tile_w;
+		const int tile_h;
+		const int screen_w;
+		const int screen_h;
+		const int cam_total_tiles_x;
+		const int cam_total_tiles_y;
+		const int sidebar_size;
+};
+
+gui::gui(int x, int y, const map& mm, const std::vector<const char*>& terrain_files,
+		const std::vector<const char*>& unit_files)
+	: m(mm),
+	tile_w(32),
+	tile_h(32),
+	screen_w(x),
+	screen_h(y),
+	cam_total_tiles_x((screen_w + tile_w - 1) / tile_w),
+	cam_total_tiles_y((screen_h + tile_h - 1) / tile_h),
+	sidebar_size(4)
+{
+	screen = SDL_SetVideoMode(x, y, 32, SDL_SWSURFACE);
+	if (!screen) {
+		fprintf(stderr, "Unable to set %dx%d video: %s\n", x, y, SDL_GetError());
+		return;
+	}
+	if(screen->locked) {
+		fprintf(stderr, "screen is locked\n");
+		SDL_UnlockSurface(screen);
+	}
+	terrains.resize(terrain_files.size());
+	for(unsigned int i = 0; i < terrain_files.size(); i++) {
+		terrains[i] = sdl_load_image(terrain_files[i]);
+	}
+	unit_images.resize(unit_files.size());
+	for(unsigned int i = 0; i < unit_files.size(); i++) {
+		unit_images[i] = sdl_load_image(unit_files[i]);
+	}
+	cam.cam_x = cam.cam_y = 0;
+}
+
+gui::~gui()
+{
+	for(unsigned int i = 0; i < terrains.size(); i++) {
+		SDL_FreeSurface(terrains[i]);
+	}
+}
+
+int gui::show_terrain_image(int x, int y, int xpos, int ypos) const
+{
+	SDL_Rect dest;
+	dest.x = xpos * tile_w;
+	dest.y = ypos * tile_h;
+	int val = m.get_data(x, y);
+	if(val < 0 || val >= (int)terrains.size()) {
+		fprintf(stderr, "Terrain at %d not loaded at (%d, %d)\n", val,
+				x, y);
+		return 1;
+	}
+	if(SDL_BlitSurface(terrains[val], NULL, screen, &dest)) {
+		fprintf(stderr, "Unable to blit surface: %s\n", SDL_GetError());
+		return 1;
+	}
+	return 0;
+}
+
+int gui::display() const
+{
+	if (SDL_MUSTLOCK(screen)) {
+		if (SDL_LockSurface(screen) < 0) {
+			return 1;
+		}
+	}
+	draw_sidebar();
+	if (SDL_MUSTLOCK(screen)) {
+		SDL_UnlockSurface(screen);
+	}
+	draw_main_map();
+	if(SDL_Flip(screen)) {
+		fprintf(stderr, "Unable to flip: %s\n", SDL_GetError());
+		return 1;
+	}
+	return 0;
+}
+
 int gui::draw_sidebar() const
 {
 	const int minimap_w = sidebar_size * tile_w;
@@ -267,6 +311,25 @@ int gui::draw_sidebar() const
 	return 0;
 }
 
+int gui::draw_unit(const unit& u) const
+{
+	if(u.unit_id < 0 || u.unit_id >= (int)unit_images.size()) {
+		fprintf(stderr, "Image for Unit ID %d not loaded\n", u.unit_id);
+		return 1;
+	}
+	if(in_bounds(cam.cam_x, u.xpos, cam.cam_x + cam_total_tiles_x) &&
+	   in_bounds(cam.cam_y, u.ypos, cam.cam_y + cam_total_tiles_y)) {
+		SDL_Rect dest;
+		dest.x = (u.xpos + sidebar_size - cam.cam_x) * tile_w;
+		dest.y = (u.ypos - cam.cam_y) * tile_h;
+		if(SDL_BlitSurface(unit_images[u.unit_id], NULL, screen, &dest)) {
+			fprintf(stderr, "Unable to blit surface: %s\n", SDL_GetError());
+			return 1;
+		}
+	}
+	return 0;
+}
+
 int gui::draw_main_map() const
 {
 	int imax = std::min(cam.cam_y + cam_total_tiles_y, m.size_y);
@@ -276,6 +339,11 @@ int gui::draw_main_map() const
 			if(show_terrain_image(j, i, x, y)) {
 				return 1;
 			}
+		}
+	}
+	for(std::vector<unit*>::const_iterator it = m.units.begin(); it != m.units.end(); ++it) {
+		if(draw_unit(**it)) {
+			return 1;
 		}
 	}
 	return 0;
@@ -330,26 +398,34 @@ color gui::get_minimap_color(int x, int y) const
 
 int run()
 {
+	std::vector<unit*> units;
+	units.push_back(new unit(0, 1, 1, color(255, 0, 0)));
+
 	std::vector<const char*> terrain_files;
+	std::vector<const char*> unit_files;
 	terrain_files.push_back("share/terrain1.png");
 	terrain_files.push_back("share/terrain2.png");
-	map m(64, 32);
-	gui g(640, 480, m, terrain_files);
+	unit_files.push_back("share/settlers.png");
+	bool running = true;
+
+	map m(64, 32, units);
+	gui g(640, 480, m, terrain_files, unit_files);
 	g.display();
-	while(1) {
+	while(running) {
 		SDL_Event event;
 		SDL_WaitEvent(&event);
 		switch(event.type) {
 			case SDL_KEYDOWN:
 				if(g.handle_keydown(event.key.keysym.sym))
-					return 0;
+					running = false;
 				break;
 			case SDL_QUIT:
-				return 0;
+				running = false;
 			default:
 				break;
 		}
 	}
+	return 0;
 }
 
 int main(int argc, char **argv)
